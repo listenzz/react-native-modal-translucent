d682db4ae6507fbcfb97d93fa76f848f481767f4
Listen <listenzz@163.com>
2018-04-23 15:47:01 +0800

    fix postinstall script

--- scripts/translucent-modal.js
+++ scripts/translucent-modal.js
@@ -1,10 +1,11 @@
 const fs = require('fs-extra');
+const path = require('path');
 
 // 找到 modal 文件
-const modal = './node_modules/react-native/Libraries/Modal/Modal.js';
+const modal = path.resolve(__dirname, '../../react-native/Libraries/Modal/Modal.js')
 
 fs.readFile(modal, 'utf8', function(err, data) { 
-  let str = data.replace(/(requireNativeComponent\('RCTModalHostView', null\))/gm, 
-  `Platform.OS === 'ios' ? $1 : requireNativeComponent('TranslucentModalHostView', null)`)
+  let str = data.replace(/(const RCTModalHostView = )(requireNativeComponent\('RCTModalHostView', null\))/gm, 
+  `$1Platform.OS === 'ios' ? $2 : requireNativeComponent('TranslucentModalHostView', null)`)
   fs.outputFile(modal, str)
 });

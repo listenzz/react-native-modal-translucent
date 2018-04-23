83119ee5c308b54cfdd3d0e817432d37070ca964
Listen <listenzz@163.com>
2018-04-23 15:46:39 +0800

    remove support for Android 4.4

--- android/src/main/java/me/listenzz/modal/TranslucentModalHostView.java
+++ android/src/main/java/me/listenzz/modal/TranslucentModalHostView.java
@@ -34,7 +34,7 @@ public class TranslucentModalHostView extends ReactModalHostView {
         Dialog dialog = getDialog();
         if (dialog != null) {
             setStatusBarTranslucent(dialog.getWindow(), true);
-            setStatusBarColor(dialog.getWindow(), Color.TRANSPARENT, false);
+            setStatusBarColor(dialog.getWindow(), Color.TRANSPARENT);
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
                 setStatusBarStyle(dialog.getWindow(), isDark());
             }
@@ -66,93 +66,15 @@ public class TranslucentModalHostView extends ReactModalHostView {
             } else {
                 decorView.setOnApplyWindowInsetsListener(null);
             }
-
             ViewCompat.requestApplyInsets(decorView);
-        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-            if (translucent) {
-                window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-            } else {
-                window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-            }
-            ViewCompat.requestApplyInsets(window.getDecorView());
         }
     }
 
-    public static void setStatusBarColor(final Window window, int color, boolean animated) {
+    public static void setStatusBarColor(final Window window, int color) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-            if (animated) {
-                int curColor = window.getStatusBarColor();
-                ValueAnimator colorAnimation = ValueAnimator.ofObject(new ArgbEvaluator(), curColor, color);
-
-                colorAnimation.addUpdateListener(
-                        new ValueAnimator.AnimatorUpdateListener() {
-                            @TargetApi(21)
-                            @Override
-                            public void onAnimationUpdate(ValueAnimator animator) {
-                                window.setStatusBarColor((Integer) animator.getAnimatedValue());
-                            }
-                        });
-                colorAnimation.setDuration(200).setStartDelay(0);
-                colorAnimation.start();
-            } else {
-                window.setStatusBarColor(color);
-            }
-        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-            ViewGroup decorViewGroup = (ViewGroup) window.getDecorView();
-            View statusBarView = decorViewGroup.findViewWithTag("custom_status_bar_tag");
-            if (statusBarView == null) {
-                statusBarView = new View(window.getContext());
-                statusBarView.setTag("custom_status_bar_tag");
-                FrameLayout.LayoutParams params = new FrameLayout.LayoutParams(
-                        FrameLayout.LayoutParams.MATCH_PARENT, getStatusBarHeight(window.getContext()));
-                params.gravity = Gravity.TOP;
-                statusBarView.setLayoutParams(params);
-                decorViewGroup.addView(statusBarView);
-            }
-
-            if (animated) {
-                Drawable drawable = statusBarView.getBackground();
-                int curColor = Integer.MAX_VALUE;
-                if (drawable != null && drawable instanceof ColorDrawable) {
-                    ColorDrawable colorDrawable = (ColorDrawable) drawable;
-                    curColor = colorDrawable.getColor();
-                }
-                if (curColor != Integer.MAX_VALUE) {
-                    final View barView = statusBarView;
-                    ValueAnimator colorAnimation = ValueAnimator.ofObject(new ArgbEvaluator(), curColor, color);
-                    colorAnimation.addUpdateListener(
-                            new ValueAnimator.AnimatorUpdateListener() {
-                                @Override
-                                public void onAnimationUpdate(ValueAnimator animator) {
-                                    barView.setBackground(new ColorDrawable((Integer) animator.getAnimatedValue()));
-                                }
-                            });
-                    colorAnimation.setDuration(200).setStartDelay(0);
-                    colorAnimation.start();
-                } else {
-                    statusBarView.setBackground(new ColorDrawable(color));
-                }
-            } else {
-                statusBarView.setBackground(new ColorDrawable(color));
-            }
-        }
-    }
-
-    private static int statusBarHeight = -1;
-
-    public static int getStatusBarHeight(Context context) {
-        if (statusBarHeight != -1) {
-            return statusBarHeight;
-        }
-
-        //获取status_bar_height资源的ID
-        int resourceId = context.getResources().getIdentifier("status_bar_height", "dimen", "android");
-        if (resourceId > 0) {
-            //根据资源ID获取响应的尺寸值
-            statusBarHeight = context.getResources().getDimensionPixelSize(resourceId);
-        }
-        return statusBarHeight;
+            window.setStatusBarColor(color);
+        } 
     }
 
     public static void setStatusBarStyle(Window window, boolean dark) {
@@ -162,5 +84,4 @@ public class TranslucentModalHostView extends ReactModalHostView {
                     dark ? View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR : 0);
         }
     }
-
 }

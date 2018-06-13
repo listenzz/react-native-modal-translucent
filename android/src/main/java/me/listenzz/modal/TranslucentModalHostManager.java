5e1d100b32f214f0c7315429366a06cf24b826f5
Listen <listenzz@163.com>
2018-06-13 12:00:45 +0800

    try to fix #3

--- android/src/main/java/me/listenzz/modal/TranslucentModalHostManager.java
+++ android/src/main/java/me/listenzz/modal/TranslucentModalHostManager.java
@@ -1,6 +1,7 @@
 package me.listenzz.modal;
 
 import com.facebook.react.module.annotations.ReactModule;
+import com.facebook.react.uimanager.LayoutShadowNode;
 import com.facebook.react.uimanager.ThemedReactContext;
 import com.facebook.react.views.modal.ReactModalHostManager;
 import com.facebook.react.views.modal.ReactModalHostView;
@@ -17,5 +18,15 @@ public class TranslucentModalHostManager extends ReactModalHostManager {
     @Override
     protected ReactModalHostView createViewInstance(ThemedReactContext reactContext) {
         return new TranslucentModalHostView(reactContext);
+    }
+
+    @Override
+    public LayoutShadowNode createShadowNodeInstance() {
+        return new LayoutShadowNode();
+    }
+
+    @Override
+    public Class<? extends LayoutShadowNode> getShadowNodeClass() {
+        return LayoutShadowNode.class;
     }
 }

87942974c2d4b1b405bd08069bcb35d8fdb19959
Listen <listenzz@163.com>
2018-11-21 17:31:15 +0800

    fix issue #7

--- android/src/main/java/me/listenzz/modal/TranslucentModalHostShadowNode.java
+++ android/src/main/java/me/listenzz/modal/TranslucentModalHostShadowNode.java
@@ -11,7 +11,7 @@ public class TranslucentModalHostShadowNode extends LayoutShadowNode {
     @Override
     public void addChildAt(ReactShadowNodeImpl child, int i) {
         super.addChildAt(child, i);
-        DisplayMetrics screenDisplayMetrics = DisplayMetricsHolder.getScreenDisplayMetrics();
+        DisplayMetrics screenDisplayMetrics = DisplayMetricsHolder.getWindowDisplayMetrics();
         child.setStyleWidth(screenDisplayMetrics.widthPixels);
         child.setStyleHeight(screenDisplayMetrics.heightPixels);
     }
